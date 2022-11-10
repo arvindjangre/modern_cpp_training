6f9e34e069a2db2bf0cc209f6ea084ebe5d67179
ITT_arvind_jangre <arvind.jangre@intimetec.com>
2022-11-10 14:06:47 +0530

    solution: calculator assignment

---
+++ 1_week/calculator/main.cpp
@@ -0,0 +1,20 @@
+/*
+Calculator Assignment
+*/
+#include "calculator.hpp"
+
+int main() {
+  int choice;
+  char ch = 'y';
+  do {
+    system("clear");
+    std::cout << "Enter your choice:\n";
+    show_choices();
+    std::cin >> choice;
+    process(choice);
+    std::cout << "Do you want to continue(y/Y): ";
+    std::cin >> ch;
+  } while(ch == 'y' | ch == 'Y');
+
+  return 0;
+}
