a74383e3849a2d2232cb098c123bf836ad3bf819
ITT_arvind_jangre <arvind.jangre@intimetec.com>
2023-02-01 18:12:20 +0530

    added more tests

--- 5_week/test/TestPlayer.cpp
+++ 5_week/test/TestPlayer.cpp
@@ -29,3 +29,39 @@ TEST_F(PlayerTest, CreatePlayListTest) {
   player_.CreatePlaylist(playlist_name);
   ASSERT_TRUE(player_.getCount() == 1);
 }
+
+TEST_F(PlayerTest, PlayTest) {
+  std::string playlist_name = "first";
+  player_.CreatePlaylist(playlist_name);
+  player_.AddSongToPlaylist(playlist_name, Song("one"));
+  play_list_.addSong(Song("two"));
+  EXPECT_CALL(play_list_, Play()).Times(AtLeast(1));
+  play_list_.Play();
+}
+
+TEST_F(PlayerTest, PauseTest) {
+ std::string playlist_name = "first";
+  player_.CreatePlaylist(playlist_name);
+  player_.AddSongToPlaylist(playlist_name, Song("one"));
+  play_list_.addSong(Song("two"));
+  EXPECT_CALL(play_list_, Pause()).Times(AtLeast(1));
+  play_list_.Pause();
+}
+
+TEST_F(PlayerTest, NextTest) {
+ std::string playlist_name = "first";
+  player_.CreatePlaylist(playlist_name);
+  player_.AddSongToPlaylist(playlist_name, Song("one"));
+  play_list_.addSong(Song("two"));
+  EXPECT_CALL(play_list_, Next()).Times(AtLeast(1));
+  play_list_.Next();
+}
+
+TEST_F(PlayerTest, PreviousTest) {
+ std::string playlist_name = "first";
+  player_.CreatePlaylist(playlist_name);
+  player_.AddSongToPlaylist(playlist_name, Song("one"));
+  play_list_.addSong(Song("two"));
+  EXPECT_CALL(play_list_, Previous()).Times(AtLeast(1));
+  play_list_.Previous();
+}
